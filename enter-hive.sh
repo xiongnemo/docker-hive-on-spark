fc8084513c4a4ef44f285a9288540b124a9a5332
Nemo Xiong <xiongnemo@126.com>
2021-02-03 16:33:16 +0800

    replace hardcoded container names with docker-compose ***

--- enter-hive.sh
+++ enter-hive.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it docker-hive-on-spark_nodemaster_1 /bin/bash
+docker-compose exec nodemaster /bin/bash
--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
 function stopServices {
   echo ">> Stopping Spark Master and slaves ..."
-  docker exec docker-hive-on-spark_nodemaster_1 stop-master.sh
-  docker exec docker-hive-on-spark_node2_1 stop-slave.sh
-  docker exec docker-hive-on-spark_node3_1 stop-slave.sh
-  docker exec docker-hive-on-spark_nifi_1 /home/hadoop/nifi/bin/nifi.sh stop
-  docker exec docker-hive-on-spark_zeppelin_1 /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
+  docker-compose exec dnodemaster stop-master.sh
+  docker-compose exec node2 stop-slave.sh
+  docker-compose exec node3 stop-slave.sh
+  docker-compose exec nifi /home/hadoop/nifi/bin/nifi.sh stop
+  docker-compose exec zeppelin /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
   echo ">> Stopping containers ..."
   docker-compose stop
 }
