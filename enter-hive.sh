9efac0dcac98579ad1eb569278768abb97ef8fc8
Nemo Xiong <xiongnemo@126.com>
2021-02-03 16:01:44 +0800

    fix: container name

--- enter-hive.sh
+++ enter-hive.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it docker_hive_nodemaster_1 /bin/bash
+docker exec -it docker-hive-on-spark_nodemaster_1 /bin/bash
--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
 function stopServices {
   echo ">> Stopping Spark Master and slaves ..."
-  docker exec docker_hive_nodemaster_1 stop-master.sh
-  docker exec docker_hive_node2_1 stop-slave.sh
-  docker exec docker_hive_node3_1 stop-slave.sh
-  docker exec docker_hive_nifi_1 /home/hadoop/nifi/bin/nifi.sh stop
-  docker exec docker_hive_zeppelin_1 /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
+  docker exec docker-hive-on-spark_nodemaster_1 stop-master.sh
+  docker exec docker-hive-on-spark_node2_1 stop-slave.sh
+  docker exec docker-hive-on-spark_node3_1 stop-slave.sh
+  docker exec docker-hive-on-spark_nifi_1 /home/hadoop/nifi/bin/nifi.sh stop
+  docker exec docker-hive-on-spark_zeppelin_1 /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
   echo ">> Stopping containers ..."
   docker-compose stop
 }
