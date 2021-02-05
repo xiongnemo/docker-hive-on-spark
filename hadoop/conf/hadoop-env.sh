e1378d63996bd0127dd0c547fe75bedafddd802e
Nemo Xiong <xiongnemo@126.com>
2021-02-05 20:20:30 +0800

    optimize: optimize dockerfile, mount more conf directories; optimize hive on spark

---
+++ hadoop/conf/hadoop-env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/home/hadoop/hadoop
+export HADOOP_CONF_DIR=/home/hadoop/hadoop/etc/hadoop
+export HDFS_NAMENODE_USER=hadoop
+export HDFS_DATANODE_USER=hadoop
+export HDFS_SECONDARYNAMENODE_USER=hadoop
