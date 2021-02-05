0c28332ec91e37c6f66e5ba89653f0580ef21f9d
Nemo Xiong <xiongnemo@126.com>
2021-02-05 20:24:07 +0800

    use hadoop user to stop services

--- stop.sh
+++ stop.sh
@@ -1,10 +1,13 @@
 function stopServices {
-  echo ">> Stopping Spark Master and slaves ..."
-  docker-compose exec nodemaster stop-master.sh
-  docker-compose exec node2 stop-slave.sh
-  docker-compose exec node3 stop-slave.sh
-  docker-compose exec nifi /home/hadoop/nifi/bin/nifi.sh stop
-  docker-compose exec zeppelin /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
+  echo ">> Stopping Spark Master..."
+  docker-compose exec -u hadoop nodemaster stop-master.sh
+  echo ">> Stopping Spark slaves..."
+  docker-compose exec -u hadoop node2 stop-slave.sh
+  docker-compose exec -u hadoop node3 stop-slave.sh
+  echo ">> Stopping Nifi..."
+  docker-compose exec -u hadoop nifi /home/hadoop/nifi/bin/nifi.sh stop
+  echo ">> Stopping Zeppelin..."
+  docker-compose exec -u hadoop zeppelin /home/hadoop/zeppelin/bin/zeppelin-daemon.sh stop
   echo ">> Stopping containers ..."
   docker-compose stop
 }
