174dfe31775460ba6bfa3e6be8c2b2d3e907d150
Nemo Xiong <xiongnemo@126.com>
2021-02-07 15:43:00 +0800

    user python3 for pyspark

--- zeppelin/entrypoint.sh
+++ zeppelin/entrypoint.sh
@@ -2,6 +2,9 @@
 service ssh start
 sudo -i -u hadoop bash << EOF
 echo "anzupop:nifi: Starting Zeppelin..."
+export PYSPARK_PYTHON=python3
+export PYSPARK_DRIVER_PYTHON=python3
+export ZEPPELIN_PYTHON=python3
 /home/hadoop/zeppelin/bin/zeppelin-daemon.sh start
 sleep 1145141919810d
 EOF
